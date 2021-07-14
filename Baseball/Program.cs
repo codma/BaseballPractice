f31eebb6fe50d2d295bbf9222c1a421b4faee1fd
Sydney <jinjoolee.0319@gmil.com>
2021-07-14 21:27:32 +0900

    최초생성

---
+++ Baseball/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baseball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
